5bfb990dbb070ba517d743a05c1fea3f927cb6a0
elieve <bayustiadi@ymail.com>
2022-08-15 19:03:12 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/eliebe/m",
+    "https://github.com/elieve/m",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
